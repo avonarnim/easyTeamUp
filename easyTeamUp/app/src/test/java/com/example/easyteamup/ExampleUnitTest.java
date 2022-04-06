38404a344ef5e2c28816ee151d6e86b47bfa74df
Nisha Chatwani <nchatwan@usc.edu>
2022-04-06 12:33:11 -0700

    sample espresso file

--- easyTeamUp/app/src/test/java/com/example/easyteamup/ExampleUnitTest.java
+++ easyTeamUp/app/src/test/java/com/example/easyteamup/ExampleUnitTest.java
@@ -1,17 +1,22 @@
 package com.example.easyteamup;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void sampleUnitTest() {
+        assertTrue(true);
     }
 }
